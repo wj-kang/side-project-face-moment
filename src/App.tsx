869eadd8735d30aff6fabcd5f9dfc639a4c6d5b2
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-07-25 22:58:55 -0500

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome!</h1>
+      <Routes>
+        <Route path="/" element={<h3>path /</h3>} />
+        <Route path="/create-room" element={<h3>new room</h3>} />
+        <Route path="/join-room/:roomId" element={<h3>new room</h3>} />
+        <Route path="/room/:roomId" element={<h3>meeting room</h3>} />
+      </Routes>
     </div>
   );
 }
