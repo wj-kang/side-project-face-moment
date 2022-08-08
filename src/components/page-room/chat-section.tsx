73cefa8305129b8751913093975983e76c8d9f90
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-08-07 20:29:48 -0500

    add chat section

---
+++ src/components/page-room/chat-section.tsx
@@ -0,0 +1,9 @@
+import styles from './chat-section.module.css';
+
+type Props = {};
+
+function ChatSection({}: Props) {
+  return <section className={styles.section}>chat</section>;
+}
+
+export default ChatSection;
