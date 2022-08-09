094eb193fcae94096b7368835d941cf1ef7bd73f
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-08-09 16:55:18 -0500

    room page - io join room

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,10 +1,33 @@
+import { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { SocketContext } from '../context/socket';
+import styles from './room.module.css';
+
+import { Socket } from 'socket.io-client';
 import ChatSection from '../components/page-room/chat-section';
 import VideoSection from '../components/page-room/video-section';
-import styles from './room.module.css';
 
 type Props = {};
 
 function RoomPage({}: Props) {
+  const socket: Socket = useContext(SocketContext);
+  const { roomId } = useParams();
+
+  useEffect(() => {
+    socket.connect().emit('join_room', roomId, handleJoinRoom);
+  }, []);
+
+  function handleJoinRoom(result: boolean): void {
+    if (result) {
+      console.log('join success');
+    } else {
+      console.log('fail');
+      // Redirect to error page
+    }
+  }
+
+  socket.on('welcome', () => console.log('good'));
+
   return (
     <>
       <div className={styles.page__container}>
