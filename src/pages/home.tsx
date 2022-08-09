6569d9d1206935600d13f46ec7d3b9aba6240ae8
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-08-09 16:54:37 -0500

    home page - create roomId with uuid and routing with param

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,11 @@
-import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { getUUID } from '../utils/uuid';
 import styles from './home.module.css';
+import { useEffect } from 'react';
 
 function HomePage() {
+  useEffect(() => {}, []);
+
   return (
     <>
       <section className={styles.section}>
@@ -11,9 +15,10 @@ function HomePage() {
             <h3>Free Video Chat</h3>
             <div>
               <div>Need to meet face-to-face virtually?</div>
-              <Button size="large" variant="contained" color="inherit">
-                Create New Room
-              </Button>
+
+              <Link to={`/room/${getUUID()}`}>
+                <button>Create New Room</button>
+              </Link>
             </div>
           </div>
           <div className={styles.right_box}>img</div>
