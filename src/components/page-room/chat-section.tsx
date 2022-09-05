996e36c97883c4e838c9564a07b64cec7db59197
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-09-05 14:58:50 -0500

    clean up files

--- src/components/page-room/chat-section.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './chat-section.module.css';
-
-type Props = {};
-
-function ChatSection({}: Props) {
-  return <section className={styles.section}>chat</section>;
-}
-
-export default ChatSection;
