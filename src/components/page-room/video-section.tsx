711886592864e0624809c3592c35000165bcfbb0
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-09-05 15:00:52 -0500

    video section

--- src/components/page-room/video-section.tsx
+++ src/components/page-room/video-section.tsx
@@ -1,24 +1,27 @@
-import { useContext, useEffect, useState } from 'react';
-import { Socket } from 'socket.io-client';
-import { SocketContext } from '../../context/socket';
+import { useEffect, useState } from 'react';
 import MediaStreamManager from '../../utils/media-stream-manager';
+import WebRTCManager from '../../utils/web-rtc-manager';
+import FlipIcon from '../icons/flip-icon';
 import VideoMenu from './video-menu';
 import styles from './video-section.module.css';
 
 type Props = {
-  streamManager: MediaStreamManager;
+  streamManager: MediaStreamManager | undefined;
+  rtcManager: WebRTCManager | undefined;
+  isConnected: boolean;
 };
 
-function VideoSection({ streamManager }: Props) {
-  const socket: Socket = useContext(SocketContext);
+function VideoSection({ streamManager, rtcManager, isConnected }: Props) {
   const [audioStatus, setAudioStatus] = useState<boolean>(false);
   const [videoStatus, setVideoStatus] = useState<boolean>(false);
 
   useEffect(() => {
+    if (!streamManager) return;
     streamManager.toggleCurrentAudioStatus();
   }, [audioStatus]);
 
   useEffect(() => {
+    if (!streamManager) return;
     streamManager.toggleCurrentVideoStatus();
   }, [videoStatus]);
 
@@ -30,13 +33,43 @@ function VideoSection({ streamManager }: Props) {
     setAudioStatus((prev) => !prev);
   }
 
+  function handleChangeVideoPosition() {
+    const myVideo: HTMLVideoElement = document.querySelector('#video__my')!;
+    const myVideoSrc = myVideo.srcObject;
+    const peerVideo: HTMLVideoElement = document.querySelector('#video__peer')!;
+    const peerVideoSrc = peerVideo.srcObject;
+    myVideo.id = 'video__peer';
+    myVideo.srcObject = peerVideoSrc;
+    peerVideo.id = 'video__my';
+    peerVideo.srcObject = myVideoSrc;
+  }
+
   return (
     <section className={styles.section}>
       <div className={styles.video__container}>
-        <video id="video__my" autoPlay={true} playsInline={true} className={styles.video__stream__my}></video>
-        {/* <video id="video__peer" className={styles.video__stream__peer}></video> */}
+        <div className={styles.video__message__box}>
+          {isConnected ? (
+            <>
+              <div className={styles.video__message__sign__on}></div>
+              <div>Connected</div>
+            </>
+          ) : (
+            <>
+              <div className={styles.video__message__sign__off}></div>
+              <div>No user connection</div>
+            </>
+          )}
+        </div>
+
+        <video id="video__peer" autoPlay={true} playsInline={true} className={styles.video__stream}></video>
+
+        <div className={styles.float__container} onClick={handleChangeVideoPosition}>
+          <FlipIcon width="1rem" height="1rem" />
+          <video id="video__my" autoPlay={true} playsInline={true} className={styles.video__stream__float}></video>
+        </div>
       </div>
       <VideoMenu //
+        streamManager={streamManager}
         audioStatus={audioStatus}
         videoStatus={videoStatus}
         handleChangeVideoStatus={handleChangeVideoStatus}
--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,42 +1,41 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { SocketContext } from '../context/socket';
 import MediaStreamManager from '../utils/media-stream-manager';
 import styles from './room.module.css';
 
 import { Socket } from 'socket.io-client';
-import ChatSection from '../components/page-room/chat-section';
 import VideoSection from '../components/page-room/video-section';
+import WebRTCManager from '../utils/web-rtc-manager';
 
-type Props = {};
-
-function RoomPage({}: Props) {
-  const streamManager = new MediaStreamManager();
+function RoomPage() {
+  const roomId = useParams().roomId || '';
   const socket: Socket = useContext(SocketContext);
-  const { roomId } = useParams();
+  const [streamManager, setStreamManager] = useState<MediaStreamManager>();
+  const [rtcManager, setRtcManager] = useState<WebRTCManager>();
+  const [connection, setConnection] = useState<boolean>(false);
 
   useEffect(() => {
-    socket.connect().emit('join_room', roomId, (result: boolean) => {
-      if (result) {
-        console.log('join success');
-      } else {
-        console.log('fail');
-        // Redirect to error page
-      }
-    });
-
-    // init media stream
-    streamManager
-      .getMedia() //
-      .then(() => streamManager.setStreamIntoElement(document.querySelector('#video__my')!));
+    MediaStreamManager.getMedia() //
+      .then((initialStream) => {
+        setStreamManager(new MediaStreamManager(initialStream));
+        setRtcManager(new WebRTCManager(socket, initialStream, roomId, setConnection));
+      })
+      .then(() => {
+        socket.emit('join_room', roomId, (result: boolean) => {
+          if (!result) {
+            console.log('Fail, Room Full');
+          }
+        });
+      });
   }, []);
 
   return (
     <>
       <div className={styles.page__container}>
         <div className={styles.section__container}>
-          <VideoSection streamManager={streamManager} />
-          <ChatSection />
+          <VideoSection streamManager={streamManager} rtcManager={rtcManager} isConnected={connection} />
+          {/* <ChatSection /> */}
         </div>
       </div>
     </>
